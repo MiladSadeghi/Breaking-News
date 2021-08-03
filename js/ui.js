6e4b02a91626b212f29fb35808a6b4b90b5f588d
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-08-03 17:24:35 +0430

    result is displayed when fetch is returned

--- js/ui.js
+++ js/ui.js
@@ -10,7 +10,7 @@ class HTMLUI {
   }
 
   showResult(news) {
-    console.log(news);
+    document.querySelector('.res').style.display = 'block'
     const resContent = document.querySelector('#res-content')
     const articles = news.articles
     let title;
