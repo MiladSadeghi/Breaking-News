014ba1acb40f99261c1fe9346623eaf7a69e4bd7
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-08-03 21:50:00 +0430

    if endpoints value change, all field will be change

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const newsAPI = new News()
 
 // variable
 const form = document.querySelector('#form')
+const endPointsSelect = document.querySelector('#EndPoints')
 let articles;
 
 
@@ -15,6 +16,10 @@ function eventlisteners() {
   document.querySelector('#From').addEventListener('change', () => {
     let today = new Date().toLocaleDateString('en-CA')
     document.querySelector('#To').value = today
+  })
+
+  endPointsSelect.addEventListener('change', () => {
+    html.changeMain(endPointsSelect.value)
   })
 }
 
