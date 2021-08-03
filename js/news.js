adf11fc249463f2d84977170ca6dd3bfc5a82ad7
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-08-04 01:14:26 +0430

    create api for top-headlines endpoints

--- js/news.js
+++ js/news.js
@@ -3,41 +3,66 @@ class News {
     this.API = 'df048b509f3143c1bab0389b4fdbac4a'
   }
 
-  async makeAndGetFromAPI(nTQ, nT, d, eD, dF, dT, l, sB) {
-    let apiURL = 'https://newsapi.org/v2/everything?'
+  async makeAndGetFromAPI(endPoints ,nTQ, nT, d, eD, dF, dT, l, sB) {
+    if(endPoints === 'everything') {
+      let apiURL = 'https://newsapi.org/v2/everything?'
 
-    if(nTQ !== '') {
-      apiURL += `${nT}=${nTQ}`
+      if(nTQ !== '') {
+        apiURL += `${nT}=${nTQ}`
+      }
+
+      if(d !== '') {
+        apiURL += `&domains=${d}`
+      }
+
+      if(eD !== '') {
+        apiURL += `&excludeDomains=${eD}`
+      }
+
+      if(dF !== '') {
+        apiURL += `&from=${dF}`
+      }
+
+      if(dT !== '') {
+        apiURL += `&to=${dT}`
+      }
+
+      if(l !== '') {
+        apiURL += `&language=${l}`
+      }
+
+      if(sB !== '') {
+        apiURL += `&sortBy=${sB}`
+      }
+
+      apiURL += `&apiKey=${this.API}`
+
+      const newsResponse = await fetch(apiURL)
+      const news = await newsResponse.json()
+      return news
     }
+    if(endPoints === 'top-headlines') {
+      let apiURL = 'https://newsapi.org/v2/top-headlines?'
+      console.log(...arguments);
+      console.log(nTQ, nT, d);
+      if(nTQ !== '') {
+        apiURL += `q=${nTQ}`
+      }
 
-    if(d !== '') {
-      apiURL += `&domains=${d}`
+      if(nT !== '') {
+        apiURL += `&country=${nT}`
+      }
+
+      if(d !== '') {
+        apiURL += `&category=${d}`
+      }
+
+      apiURL += `&apiKey=${this.API}`
+
+      console.log(apiURL);
+      const newsResponse = await fetch(apiURL)
+      const news = await newsResponse.json()
+      return news
     }
-
-    if(eD !== '') {
-      apiURL += `&excludeDomains=${eD}`
-    }
-
-    if(dF !== '') {
-      apiURL += `&from=${dF}`
-    }
-
-    if(dT !== '') {
-      apiURL += `&to=${dT}`
-    }
-
-    if(l !== '') {
-      apiURL += `&language=${l}`
-    }
-
-    if(sB !== '') {
-      apiURL += `&sortBy=${sB}`
-    }
-
-    apiURL += `&apiKey=${this.API}`
-
-    const newsResponse = await fetch(apiURL)
-    const news = await newsResponse.json()
-    return news
   }
 }
